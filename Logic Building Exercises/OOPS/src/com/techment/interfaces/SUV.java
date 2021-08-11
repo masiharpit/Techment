5db3e24be349dce1614b543617103e586af807fc
Arpit <41959043+masiharpit@users.noreply.github.com>
2021-08-11 23:15:36 +0530

    Delete Logic Building Exercises directory

--- Logic Building Exercises/OOPS/src/com/techment/interfaces/SUV.java
+++
@@ -1,7 +0,0 @@
-package com.techment.interfaces;
-
-public class SUV {
-	public void vehicleInfo() {
-		System.out.println("SUV is 7 seater.");
-	}
-}
--- Logic Building Exercises/OOPS/src/com/techment/interfaces/Vehicle.java
+++
@@ -1,8 +0,0 @@
-package com.techment.interfaces;
-
-public interface Vehicle {
-	int MAX_SPEED = 85;
-	
-	void vehicleInfo();
-}
-
